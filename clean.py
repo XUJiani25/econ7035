8b320542547b0d524e55326276bdcd29b1baa781
XU Jiani <23451335@life.hkbu.edu.hk>
2024-02-16 07:16:48 +0800

    data cleaning script

--- clean.py
+++ clean.py
@@ -1,19 +1,18 @@
 import sys
 import pandas as pd
 def clean_data(input1, input2, output):
-    df1 = pd.read_csv(input1)
-    df2 = pd.read_csv(input2)
-    merged_df = pd.merge(df1, df2, left_on='respondent_id', right_on='id')
-    merged_df.drop('id', axis=1, inplace=True)
-    merged_df.dropna(inplace=True)
-    merged_df = merged_df[~merged_df['job'].str.contains('insurance|Insurance')]
-    merged_df.to_csv(output, index=False)
-    print(output)
+    df_contact = pd.read_csv(input1)
+    df_other = pd.read_csv(input2)
+    df_merged = pd.merge(df_contact, df_other, left_on='respondent_id', right_on='id')
+    df_merged.drop(columns=['id'], inplace=True)
+    df_merged.dropna(inplace=True)
+    df_merged = df_merged[~df_merged['job'].str.contains('insurance', case=False, na=False)]
+    df_merged.to_csv(output, index=False)
+    print(f"Data cleaned and saved to {output}")
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python clean.py input1 input2 output")
-        sys.exit(1)
-        input1 = sys.argv[1]
-        input2 = sys.argv[2]
-        output = sys.argv[3]
+        if len(sys.argv) != 4:
+            print("Usage: python clean.py <input1> <input2> <output>")
+            sys.exit(1)
+
+        input1, input2, output = sys.argv[1], sys.argv[2], sys.argv[3]
         clean_data(input1, input2, output)
